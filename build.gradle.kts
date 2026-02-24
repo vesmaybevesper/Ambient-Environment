4b94a683028d8dea20dee0764f4a5c7b89effd93
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-02-23 22:01:45 -0500

    update idea-ext

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ import java.util.*
 
 plugins {
     `java-library`
-    id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.7"
+    id("org.jetbrains.gradle.plugin.idea-ext") version "1.4.1"
 }
 
 version = GMUtils.updatingVersion(Versions.MOD)
