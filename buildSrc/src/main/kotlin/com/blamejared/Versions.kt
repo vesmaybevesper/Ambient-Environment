3f6e0624523feb2ba8fa637e8f57a6cdb7b8e1e1
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-02-23 21:33:23 -0500

    deps for snapshots

--- buildSrc/src/main/kotlin/com/blamejared/Versions.kt
+++ buildSrc/src/main/kotlin/com/blamejared/Versions.kt
@@ -1,11 +1,11 @@
 package com.blamejared
 
 object Versions {
-    const val MOD = "27.0.0"
-    const val JAVA = "21"
-    const val MINECRAFT = "1.21.11"
-    const val FABRIC_LOADER = "0.18.1"
-    const val FABRIC = "0.139.4+1.21.11"
-    const val NEO_FORGE = "21.11.0-beta"
+    const val MOD = "28.0.0"
+    const val JAVA = "25"
+    const val MINECRAFT = "26.1-snapshot-7"
+    const val FABRIC_LOADER = "0.18.4"
+    const val FABRIC = "0.143.5+26.1"
+    const val NEO_FORGE = "26.1.0.0-alpha.12+snapshot-7"
     const val NEO_FORGE_LOADER= "[4,)"
 }
